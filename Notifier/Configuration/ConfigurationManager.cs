10da1222d2b9c1175051d6749eac50d06de6dd4e
Kalun Wong <calendarw@gmail.com>
2011-08-17 00:04:51 +0800

    Parameters support for RecordCountMonitor

--- Notifier/Configuration/ConfigurationManager.cs
+++ Notifier/Configuration/ConfigurationManager.cs
@@ -48,6 +48,36 @@ namespace Notifier.Configuration
                     {
                         value = new System.Data.SqlClient.SqlConnection(value.ToString().Decrypt(Salt));
                     }
+                    else if (property.PropertyType.Equals(typeof(Dictionary<string, object>))){
+                        Dictionary<string, object> parameters = new Dictionary<string, object>();
+
+                        foreach (XElement pa in p.Elements("parameter"))
+                        {
+                            string pName = null;
+                            string pType = null;
+
+                            foreach (XAttribute a in pa.Attributes())
+                            {
+                                switch (a.Name.LocalName.ToLower())
+                                {
+                                    case "name":
+                                        pName = a.Value;
+                                        break;
+
+                                    case "type":
+                                        pType = a.Value;
+                                        break;
+                                }
+                            }
+
+                            if (pName == null)
+                                throw new NullReferenceException("name attribute is missing value in {0}");
+
+                            parameters.Add(pName, Convert.ChangeType(pa.Value, Type.GetType(string.Format(@"System.{0}", pType ?? "String"), true, true)));
+                        }
+
+                        value = parameters;
+                    }
                     else
                     {
                         if (!value.GetType().Equals(property.PropertyType))
--- NotifierTest/Configuration/ConfigurationManagerTest.cs
+++ NotifierTest/Configuration/ConfigurationManagerTest.cs
@@ -5,6 +5,7 @@ using Notifier.Model;
 using Notifier.Model.Monitor;
 using System.IO;
 using Notifier.Configuration;
+using System;
 
 namespace NotifierTest.Model
 {
@@ -37,7 +38,9 @@ namespace NotifierTest.Model
         public void FromXmlRecordCountTest()
         {
             INotificationModel model = ConfigurationManager.FromXml(recordCountTestFilePath);
-            Assert.AreEqual(1, model.Items.Count);
+            Assert.AreEqual(2, model.Items.Count);
+
+            // basic
             Assert.AreEqual(typeof(RecordCountMonitor), model.Items[0].GetType());
             RecordCountMonitor monitor = model.Items[0] as RecordCountMonitor;
             Assert.IsNotNull(monitor.DbConnection);
@@ -45,6 +48,21 @@ namespace NotifierTest.Model
                 , monitor.DbConnection.ConnectionString);
             Assert.AreEqual("SELECT * FROM TEST", monitor.CommandText);
             Assert.AreEqual(10, monitor.Limit);
+
+            // with parameters
+            Assert.AreEqual(typeof(RecordCountMonitor), model.Items[1].GetType());
+            monitor = model.Items[1] as RecordCountMonitor;
+            Assert.IsNotNull(monitor.DbConnection);
+            Assert.AreEqual("Data Source=myServerAddress;Initial Catalog=myDataBase;User Id=myUsername;Password=myPassword;"
+                , monitor.DbConnection.ConnectionString);
+            Assert.AreEqual("SELECT * FROM TEST WHERE PKEY=@PKEY", monitor.CommandText);
+            Assert.AreEqual(3, monitor.Parameters.Count);
+            Assert.AreEqual(monitor.Parameters["@PKEY"], 1);
+            Assert.AreEqual(monitor.Parameters["@STRING"], "Test String");
+            Assert.AreEqual(monitor.Parameters["@DATE"], new DateTime(2011, 1, 1, 12, 34, 56));
+            Assert.AreEqual(10, monitor.Limit);
+
+
         }
     }
 }
