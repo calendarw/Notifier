bc26b9607595af7569d322510946f4d2c3e51958
Kalun Wong <calendarw@gmail.com>
2011-09-03 10:44:28 +0800

    Support NotifyIcon.Click

---
+++ Notifier/IntegerExtension.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Notifier
+{
+    public static class IntegerExtension
+    {
+        public static string es(this int i)
+        {
+            return i > 1 ? "es" : "";
+        }
+
+        public static string Has(this int i)
+        {
+            return i > 1 ? "have" : "has";
+        }
+
+        public static string Is(this int i)
+        {
+            return i > 1 ? "are" : "is";
+        }
+
+        public static string s(this int i)
+        {
+            return i > 1 ? "s" : "";
+        }
+    }
+}
--- Notifier/View/NotifyIconBalloon.cs
+++ Notifier/View/NotifyIconBalloon.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Windows.Forms;
 using Notifier.Model;
 using Notifier.Model.Monitor;
@@ -24,13 +22,26 @@ namespace Notifier.View
                     value.ContentsUpdated += OnModelContentsUpdated;
 
                 mModel = value;
+
+                UpdateNotifyIconText(); 
             }
         }
 
         public NotifyIcon NotifyIcon
         {
             get { return mNotifyIcon; }
-            set { mNotifyIcon = value; }
+            set
+            {
+                if (mNotifyIcon != null)
+                    mNotifyIcon.Click -= OnNotifyIconClick;
+
+                if (value != null)
+                    value.Click += OnNotifyIconClick;
+
+                mNotifyIcon = value;
+
+                UpdateNotifyIconText(); 
+            }
         }
 
         public int Timeout
@@ -41,28 +52,45 @@ namespace Notifier.View
 
         protected void OnModelContentsUpdated(object sender, EventArgs e)
         {
-            if (mNotifyIcon != null)
+            ShowMessage(false);
+        }
+
+        private void OnNotifyIconClick(object sender, EventArgs e)
+        {
+            ShowMessage(true);
+        }
+
+        private void ShowMessage(bool showEmpty)
+        {
+            if (mModel == null || mNotifyIcon == null) return;
+
+            string text = string.Empty;
+
+            int i = 0;
+
+            foreach (IMonitor monitor in Model.Items)
             {
-                string text = string.Empty;
-
-                int i = 0;
-
-                foreach (IMonitor monitor in Model.Items)
+                if (monitor.ShouldNotify)
                 {
-                    if (monitor.ShouldNotify)
-                    {
-                        text = string.Format(@"{0}{1}{2}", text, string.IsNullOrEmpty(text) ? string.Empty : "\n", monitor.Caption);
-                        i++;
-                    }
+                    text = string.Format(@"{0}{1}{2}", text, string.IsNullOrEmpty(text) ? string.Empty : "\n", monitor.Caption);
+                    i++;
                 }
-
-                mNotifyIcon.Text = string.Format(@"{0} task{1} monitoring", Model.Items.Count, Model.Items.Count > 1 ? "s are" : " is");
-
-                string title = string.Format(@"{0} task{1} waiting to be done", i, i > 1 ? "s are" : " is");
-
-                if (!string.IsNullOrEmpty(text))
-                    mNotifyIcon.ShowBalloonTip(Timeout, title, text, ToolTipIcon.Info);
             }
+
+            if (showEmpty && i == 0)
+                text = "Currently no outstanding";
+            
+            string title = string.Format(@"{0} task{1} waiting to be done", i, string.Format("{0} {1}", i.s(), i.Is()));
+
+            if (!string.IsNullOrEmpty(text))
+                mNotifyIcon.ShowBalloonTip(Timeout, title, text, ToolTipIcon.Info);
+        }
+
+        private void UpdateNotifyIconText()
+        {
+            if (mModel == null || mNotifyIcon == null) return;
+
+            mNotifyIcon.Text = string.Format(@"{0} task{1} monitoring", Model.Items.Count, string.Format("{0} {1}", Model.Items.Count.s(), Model.Items.Count.Is()));
         }
     }
 }
